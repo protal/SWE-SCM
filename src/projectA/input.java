be6aea7f9a4fb41252c2d560afc4aa1038b7f20e
Tannnnnn <tan_sweetmulet@hotmail.com>
2017-09-19 10:51:38 +0700

    input

---
+++ src/projectA/input.java
@@ -0,0 +1,22 @@
+package projectA;
+
+import java.util.Scanner;
+
+public class input {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.print("Salaly : ");
+		System.out.println("Sale : ");
+		double salaly = in.nextInt();
+		double sale = in.nextInt();
+		
+		salaly salaly = new salaly() ;
+		double realSalary =  salaly.calSalaly();
+		System.out.println("total : " + );
+		
+	}
+	
+
+}
